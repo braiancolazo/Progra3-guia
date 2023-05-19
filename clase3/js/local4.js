ceab8f978166e48345ec37901cc395e27a3aac0b
114275_Colazo_Braian <114275@tecnicatura.frc.utn.edu.ar>
2023-05-19 16:11:25 -0300

    practica parcial

--- clase3/js/local4.js
+++ clase3/js/local4.js
@@ -1,13 +1,13 @@
 const array = ["hola",3.14,true];
 
-const array2 = new array(3);
+const array2 = new Array(3);
 console.log(array2)
 console.log("el array es: "+array)
 
 const numeros = [1,35,6,54,13,3,24]
-console.log("array de eros: " + numeros)
-console.log("primer elemento: "+ numeros(0))
-console.log("indice de error: "+numeros(123))
+console.log("array de numeros: " + numeros)
+console.log("primer elemento: "+ numeros[0])
+console.log("indice de error: "+numeros[123])
 numeros.push(65)//agrega un numero al array
 console.log("array actualizado "+ numeros)
 numeros.unshift(33)
